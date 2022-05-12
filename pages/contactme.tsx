e9a44c92a259c739278f687682b69fe20d6912d9
Digirange <jarques0123@gmail.com>
2022-05-12 08:41:17 -0400

    first-blog mdx file created

--- pages/contactme.tsx
+++ pages/contactme.tsx
@@ -1,18 +1,15 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
-import { Footer } from '../componenets/footer'
+import Footer from '../componenets/footer'
 import Nav from '../componenets/navbar'
 
 const Contactme = () => {
   return (
     <Box>
-          <Nav />
-          <Text align='center'>
-              Contact Page
-          </Text>
+      <Nav />
+      <Text align="center">Contact Page</Text>
       <Footer />
     </Box>
   )
 }
 
 export default Contactme
-
