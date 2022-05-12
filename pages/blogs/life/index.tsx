85e5bfa0016279e515dd81f82165539578bd951f
Digirange <jarques0123@gmail.com>
2022-05-12 11:16:21 -0400

    getStaticProps added on Life.index.tsx

--- pages/blogs/life/index.tsx
+++ pages/blogs/life/index.tsx
@@ -1,8 +1,13 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text, Grid, GridItem } from '@chakra-ui/react'
+import fs from 'fs'
+import path from 'path'
+import glob from 'glob'
+import matter from 'gray-matter'
 import Footer from '../../../componenets/footer'
 import Nav from '../../../componenets/navbar'
+import _ from 'lodash'
 
-const LifeBlogIndex = () => {
+const LifeBlogIndex = ({ posts }) => {
   return (
     <Box>
       <Nav />
@@ -12,6 +17,17 @@ const LifeBlogIndex = () => {
       <Footer />
     </Box>
   )
+}
+
+export const getStaticProps = async () => {
+  const postPaths = path.join(process.cwd(), 'posts/life')
+  const globPosts = glob.sync('**/*.mdx', { cwd: postPaths })
+  const posts = _.chain(globPosts)
+    .map((paths) => fs.readFileSync(path.join(postPaths, paths), 'utf-8'))
+    .map((x) => matter(x).data)
+    .sortBy((x) => new Date(x.date))
+    .reverse()
+    .value()
 }
 
 export default LifeBlogIndex
