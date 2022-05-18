12c1e96834acea3db46d21658dcd04760ca53e37
Digirange <jarques0123@gmail.com>
2022-05-18 11:41:07 -0400

    slug for life complete

--- pages/blogs/life/[slug].tsx
+++ pages/blogs/life/[slug].tsx
@@ -3,21 +3,30 @@ import fs from 'fs'
 import _ from 'lodash'
 import path from 'path'
 import matter from 'gray-matter'
-import { Box } from '@chakra-ui/react'
+import { Box, Text, Grid, GridItem } from '@chakra-ui/react'
 import { serialize } from 'next-mdx-remote/serialize'
 import { MDXRemote } from 'next-mdx-remote'
+import Nav from '../../../componenets/navbar'
+import Footer from '../../../componenets/footer'
 
 export default function IndexPage({ source, posts }) {
-    const {
-        scope: { title, description, url, date }
-    } = source
-    return (
-        <Box>
-            <MDXRemote {...source }/>
-        </Box>
-    )
+  const {
+    scope: { title, description, url, date },
+  } = source
+  return (
+    <Box>
+      <Nav />
+      <Box padding='50px'>
+        <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+          <GridItem colStart={2} colEnd={5}>
+            <MDXRemote {...source} />
+          </GridItem>
+        </Grid>
+          </Box>
+          <Footer/>
+    </Box>
+  )
 }
-
 
 export function getStaticPaths() {
   const postPaths = path.join(process.cwd(), 'posts/life')
@@ -42,17 +51,22 @@ export async function getStaticProps({ params }) {
     .sortBy((x) => new Date(x.date))
     .reverse()
     .slice(0, 10)
-      .value()
-    
-    const postsPath = path.join(process.cwd(), 'posts', 'life', params.slug + '.mdx')
-    const post = fs.readFileSync(postsPath, 'utf-8')
-    const { content, data } = matter(post)
-    const mdxSource = await serialize(content, { scope: data })
+    .value()
 
-    return {
-        props: {
-            source: mdxSource,
-            posts
-        }
-    }
+  const postsPath = path.join(
+    process.cwd(),
+    'posts',
+    'life',
+    params.slug + '.mdx',
+  )
+  const post = fs.readFileSync(postsPath, 'utf-8')
+  const { content, data } = matter(post)
+  const mdxSource = await serialize(content, { scope: data })
+
+  return {
+    props: {
+      source: mdxSource,
+      posts,
+    },
+  }
 }
