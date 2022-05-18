6fbc3a15d616a8802e617a499934d091164f719a
Digirange <jarques0123@gmail.com>
2022-05-18 10:19:32 -0400

    started getStaticProps

--- pages/blogs/life/[slug].tsx
+++ pages/blogs/life/[slug].tsx
@@ -3,6 +3,7 @@ import fs from 'fs'
 import _ from 'lodash'
 import path from 'path'
 import matter from 'gray-matter'
+import { Box } from '@chakra-ui/react'
 
 export function getStaticPaths() {
   const postPaths = path.join(process.cwd(), 'posts/life')
@@ -11,10 +12,23 @@ export function getStaticPaths() {
     .map((paths) => fs.readFileSync(path.join(postPaths, paths), 'utf-8'))
     .map((x) => matter(x).data)
     .map((x) => ({ params: { slug: x.url } }))
+    .value()
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps({ parmas }) {
+  const postPaths = path.join(process.cwd(), 'posts/life')
+  const globPosts = glob.sync('**/*.mdx', { cwd: postPaths })
+  const posts = _.chain(globPosts)
+    .map((paths) => fs.readFileSync(path.join(postPaths, paths), 'utf-8'))
+    .map((x) => matter(x).data)
+    .sortBy((x) => new Date(x.date))
+    .reverse()
+    .slice(0, 10)
       .value()
     
-    return {
-        paths,
-        fallback: false
-    }
+    const postPath = path.join(process.cwd())
 }
