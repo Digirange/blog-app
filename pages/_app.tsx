d9b768e66bc38dc80d298c4f9933627d4415ba45
Digirange <jarques0123@gmail.com>
2022-06-01 16:48:32 -0400

    theme.ts init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,6 @@
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
+import theme from '../theme'
 
-const colors = {
-  whiteGray: '#DEE2E6',
-  Gray: '#6C757C',
-  darkGray: '#212529',
-}
-
-const theme = extendTheme({ colors })
 
 const MyApp = ({ Component, pageProps }) => {
   return (
---
+++ theme.ts
@@ -0,0 +1,11 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  whiteGray: '#DEE2E6',
+  Gray: '#6C757C',
+  darkGray: '#212529',
+}
+
+const theme = extendTheme({ colors })
+
+export default theme
