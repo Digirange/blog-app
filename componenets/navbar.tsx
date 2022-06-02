2385cbebacc1b3b665eb4357a3366ea12ecc6c3c
Digirange <jarques0123@gmail.com>
2022-06-02 11:49:21 -0400

    navbar update

--- componenets/navbar.tsx
+++ componenets/navbar.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 import {
   Box,
-  Flex,
   Grid,
   GridItem,
   Button,
@@ -11,7 +10,6 @@ import {
   MenuItem,
   useDisclosure,
   useColorModeValue,
-  Stack,
   useColorMode,
   Image,
 } from '@chakra-ui/react'
@@ -30,63 +28,67 @@ const Nav = () => {
     <>
       <Box>
         <Box bg={useColorModeValue('gray.100', 'gray.900')}>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-            <Box>
-              <Link href="/">
-                <a>
-                  <Image
-                    alt="logo"
-                    src="../../harden.jpg"
-                    borderRadius="full"
-                    boxSize="65px"
-                    objectFit="cover"
-                  />
-                </a>
-              </Link>
-            </Box>
-            <Box padding="15px">
-              <Link href="/aboutme">About Me</Link>
-            </Box>
-            <Box padding="15px">
-              <Link href="/projects">Projects</Link>
-            </Box>
-            <Box padding="15px">
-              <Menu isOpen={isOpen}>
-                <MenuButton
-                  variant="ghost"
-                  mx={1}
-                  py={[1, 2, 2]}
-                  px={4}
-                  borderRadius={5}
-                  _hover={{ bg: useColorModeValue('gray.100', 'gray.700') }}
-                  aria-label="Courses"
-                  fontWeight="normal"
-                  onMouseEnter={onOpen}
-                  onMouseLeave={onClose}
-                  as="a"
-                  href="/blogs"
-                >
-                  Blogs {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
-                </MenuButton>
-                <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
-                  <MenuItem>
-                    <Link href="/blogs/developer">Developer</Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link href="/blogs/life">Life</Link>
-                  </MenuItem>
-                  <MenuItem>
-                    <Link href="/blogs/sports">Sports</Link>
-                  </MenuItem>
-                </MenuList>
-              </Menu>
-              {/* <Link href="/blogs">Blogs</Link> */}
-            </Box>
-            <Box padding="15px">
-              <Button onClick={toggleColorMode}>
-                {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
-              </Button>
-            </Box>
+          <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+            <GridItem colStart={2}>
+              <Box padding="15px">
+                <Link href="/aboutme">About Me</Link>
+              </Box>
+            </GridItem>
+            <GridItem colStart={3}>
+              <Box>
+                <Link href="/">
+                  <a>
+                    <Image
+                      alt="logo"
+                      src="../../harden.jpg"
+                      borderRadius="full"
+                      boxSize="65px"
+                      objectFit="cover"
+                    />
+                  </a>
+                </Link>
+              </Box>
+            </GridItem>
+            <GridItem colStart={4}>
+              <Box padding="15px">
+                <Menu isOpen={isOpen}>
+                  <MenuButton
+                    variant="ghost"
+                    mx={1}
+                    py={[1, 2, 2]}
+                    px={4}
+                    borderRadius={5}
+                    _hover={{ bg: useColorModeValue('gray.100', 'gray.700') }}
+                    aria-label="Courses"
+                    fontWeight="normal"
+                    onMouseEnter={onOpen}
+                    onMouseLeave={onClose}
+                    as="a"
+                    href="/blogs"
+                  >
+                    Blogs {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+                  </MenuButton>
+                  <MenuList onMouseEnter={onOpen} onMouseLeave={onClose}>
+                    <MenuItem>
+                      <Link href="/blogs/developer">Developer</Link>
+                    </MenuItem>
+                    <MenuItem>
+                      <Link href="/blogs/life">Life</Link>
+                    </MenuItem>
+                    <MenuItem>
+                      <Link href="/blogs/sports">Sports</Link>
+                    </MenuItem>
+                  </MenuList>
+                </Menu>
+              </Box>
+            </GridItem>
+            <GridItem colStart={5}>
+              <Box padding="15px">
+                <Button onClick={toggleColorMode}>
+                  {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
+                </Button>
+              </Box>
+            </GridItem>
           </Grid>
         </Box>
       </Box>
