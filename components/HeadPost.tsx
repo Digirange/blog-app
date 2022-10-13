8ccbf91eec7fbb1ad83c72c170d38b28db6c7cb7
Marques Stewart <Jarques0123@gmail.com>
2022-10-13 13:16:10 -0400

    new blog updated, new secondaryTag on blog

--- components/HeadPost.tsx
+++ components/HeadPost.tsx
@@ -1,23 +1,43 @@
 import { Box, Text, useColorModeValue } from '@chakra-ui/react'
 
 const HeadPost = ({ data }: any) => {
-  const greenColor = useColorModeValue('green.700', 'green.400')
-  return (
-    <Box>
+  console.log(data)
+  if (data.tagTwo) {
+    const greenColor = useColorModeValue('green.700', 'green.400')
+    return (
       <Box>
-        <Text fontWeight="bold">{data.title}</Text>
-      </Box>
-      <Box>
-        <Text>{data.description}</Text>
         <Box>
-          <Text>{data.date}</Text>
-          <Text color={greenColor}>#{data.tag}</Text>
+          <Text fontWeight="bold">{data.title}</Text>
         </Box>
-
-        <Text fontWeight="bold">{data.readTime + ' min read'}</Text>
+        <Box>
+          <Text>{data.description}</Text>
+          <Box>
+            <Text>{data.date}</Text>
+            <Text color={greenColor}>#{data.tag}</Text>
+            <Text color={greenColor}>#{data.tagTwo}</Text>
+          </Box>
+          <Text fontWeight="bold">{data.readTime + ' min read'}</Text>
+        </Box>
       </Box>
-    </Box>
-  )
+    )
+  } else {
+    const greenColor = useColorModeValue('green.700', 'green.400')
+    return (
+      <Box>
+        <Box>
+          <Text fontWeight="bold">{data.title}</Text>
+        </Box>
+        <Box>
+          <Text>{data.description}</Text>
+          <Box>
+            <Text>{data.date}</Text>
+            <Text color={greenColor}>#{data.tag}</Text>
+          </Box>
+          <Text fontWeight="bold">{data.readTime + ' min read'}</Text>
+        </Box>
+      </Box>
+    )
+  }
 }
 
 export default HeadPost
