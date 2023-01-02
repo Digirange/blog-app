7b8e87cdcc220b32cda8291639852b32d6fad6df
Marques Stewart <Jarques0123@gmail.com>
2023-01-02 12:07:33 -0500

    updated to see CWD

--- .next/static/HgFe8nEs7cjp-8ruLI19H/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fblogs","\u002F","\u002Fblogs\u002Flife","\u002Fblogs\u002Fdeveloper","\u002Fblogs\u002Fsports","\u002Fblogs\u002Fsports\u002F[slug]","\u002Fblogs\u002Fdeveloper\u002F[slug]","\u002Fblogs\u002Flife\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
